50d3e93efed49f8e884fd883554c88588b777d09
Jasmine Andresol <jandresol@jandres-ltmtc9m.internal.salesforce.com>
2025-06-09 16:18:08 -0700

    checkpoint 1

--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -1,1 +1,48 @@
+const modal = document.getElementById("playlistModal");
+const span = document.getElementsByClassName("close")[0];
 
+function openModal(playlist) {
+    // Set playlist header info
+        document.getElementById('playlist-image').src = playlist.imageUrl;
+        document.getElementById('playlist-name').innerText = playlist.name;
+        document.getElementById('creator-name').innerText = playlist.creator;
+
+    // Clear any existing songs
+        const songsContainer = document.getElementById('songs');
+        songsContainer.innerHTML = '';
+
+    // Add each song to the modal
+    playlist.songs.forEach(song => {
+        const songCard = document.createElement('div');
+        songCard.className = 'song-card';
+
+        songCard.innerHTML = `
+        <div class="song-info">
+            <div class="song-thumbnail"></div>
+            <div class="song-details">
+            <p class="song-title">${song.title}</p>
+            <p class="song-artist">${song.artist}</p>
+            <p class="song-album">${song.album}</p>
+            </div>
+        </div>
+        <div class="song-duration">${song.duration || '0:00'}</div>
+        `;
+
+        songsContainer.appendChild(songCard);
+    });
+
+    // Show the modal
+    modal.style.display = "block";
+    }
+
+    // Close modal on (x)
+    span.onclick = function () {
+        modal.style.display = "none";
+    }
+
+// Close modal on outside click
+    window.onclick = function (event) {
+        if (event.target === modal) {
+            modal.style.display = "none";
+        }
+    }
