3dc43f94eead874644af4aab7c989a9d13907a7b
Jasmine Andresol <jandresol@jandres-ltmtc9m.internal.salesforce.com>
2025-06-11 13:25:50 -0700

    no gitignore

---
+++ music-playlist-creator/config.js
@@ -0,0 +1,5 @@
+// curl -X POST "https://accounts.spotify.com/api/token" \
+//   -H "Authorization: Basic $(echo -n '12fd615831f443db8476ed4d1e28d464:db195a884b7d4297a75d20c0113ce7b8' | base64)" \
+//   -d "grant_type=client_credentials"
+
+export const SPOTIFY_TOKEN = 'BQAMHV_AIEL7ti7GlVpqJEuYfIcoFXAhUUjC4GcmR06lKECnslHRmjVsYVVuvDz9N27Gj3fp4ef4HlPMudB_7a2rgiUqaTlEfwGmiWcnP2xmTW8PZxcMV2nC3eQb_vgyGdUgUe1j694'
