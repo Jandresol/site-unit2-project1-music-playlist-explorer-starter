93e16d4d00afa8cc91a0fad9cc35ac532eed7153
Jasmine Andresol <jandresol@jandres-ltmtc9m.internal.salesforce.com>
2025-06-11 14:07:20 -0700

    new token

--- music-playlist-creator/config.js
+++ music-playlist-creator/config.js
@@ -2,4 +2,4 @@
 //   -H "Authorization: Basic $(echo -n '12fd615831f443db8476ed4d1e28d464:db195a884b7d4297a75d20c0113ce7b8' | base64)" \
 //   -d "grant_type=client_credentials"
 
-export const SPOTIFY_TOKEN = 'BQAMHV_AIEL7ti7GlVpqJEuYfIcoFXAhUUjC4GcmR06lKECnslHRmjVsYVVuvDz9N27Gj3fp4ef4HlPMudB_7a2rgiUqaTlEfwGmiWcnP2xmTW8PZxcMV2nC3eQb_vgyGdUgUe1j694'
+export const SPOTIFY_TOKEN = 'BQCxrlmtQ6EcidnSNMSyArKVo51glzyfuBfoB039TTX6w-3MWCsX9q2_O3MIGtrOW6rWat-3lgPBZGnwX9vByvcBpYfRoY62qrzoHdfWMfWQOgGXijM80dgId9kEVb-eN1Osc6TuA0I'
